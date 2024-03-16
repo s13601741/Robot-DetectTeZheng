98438c8316d71ca7746f7bad15a7ee6f0f9febbe
s13601741 <46368635+s13601741@users.noreply.github.com>
2024-03-16 21:24:10 +0800

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -1,20 +1,32 @@
+import cv2 
+from PIL import Image
 from transformers import ViltProcessor, ViltForQuestionAnswering
 import requests
-from PIL import Image
+vid = cv2.VideoCapture(0) 
 
-# prepare image + question
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-text = "How many cats are there?"
+while(True): 
+    ret, frame = vid.read() 
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(1) & 0xFF 
+    if key == ord('q'):
+        break
+    
+    if key == ord('s'):
+        color_coverted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+        pil_image = Image.fromarray(color_coverted)
+        text = input("Question:")
+        processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        # prepare inputs
+        encoding = processor(pil_image, text, return_tensors="pt")
 
-# prepare inputs
-encoding = processor(image, text, return_tensors="pt")
+        # forward pass
+        outputs = model(**encoding)
+        logits = outputs.logits
+        idx = logits.argmax(-1).item()
+        print("Predicted answer:", model.config.id2label[idx])
+        
+vid.release() 
+cv2.destroyAllWindows() 
 
-# forward pass
-outputs = model(**encoding)
-logits = outputs.logits
-idx = logits.argmax(-1).item()
-print("Predicted answer:", model.config.id2label[idx])
